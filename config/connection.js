d9a031a84d792f374b5cda983675f5396e9d26a7
Syed Abedi <hasnain436@gmail.com>
2020-10-11 19:24:56 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection({ 
     host: "localhost",
     user: "root",
-    password: "Fa7Hp9Gq6",
+    password: "",
     database: "burgers_db"
     });
 };
